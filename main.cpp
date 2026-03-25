e2d4f4d65825ae43f22b1c8ce7c1099270cde5cc
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-25 15:22:50 +0300

    Add case_t

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ bool testConstructAndDestruct(const char ** pname) {
   return true;
 }
 
-bool testDefaultVectorIsEmpty(const char ** pname ) {
+bool testDefaultVectorIsEmpty(const char ** pname) {
   *pname = __func__;
   Vector< int > v;
   return v.isEmpty();
@@ -17,16 +17,22 @@ bool testDefaultVectorIsEmpty(const char ** pname ) {
 
 int main() {
   using test_t = bool(*)(const char **);
-  test_t tests[] = {
-    testConstructAndDestruct,
-    testDefaultVectorIsEmpty
+  using case_t = std::pair< test_t, const char * >;
+  case_t tests[] = {
+    { testConstructAndDestruct, "Vector must be default constructable" },
+    { testDefaultVectorIsEmpty, "Default constructed vector must be empty" }
   };
-  constexpr size_t count = sizeof(tests) / sizeof(test_t);
+  constexpr size_t count = sizeof(tests) / sizeof(case_t);
+  size_t failed = 0;
   for (size_t i = 0; i < count; ++i) {
     const char * testName = nullptr;
-    bool r = tests[i](&testName);
+    bool r = tests[i].first(&testName);
     if (!r) {
+        ++failed;
       std::cout << "Failed: " << testName << "\n";
+      std:: cout << "\t" << tests[i].second << "\n";
     }
   }
+  std::cout << "Summary:\n\t" << (count - failed) << " passed\n";
+  std::cout << "\t" << count << " total\n";
 }
